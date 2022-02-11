b82ae7dfa662dce241115a5474ee576cdb1bc642
Ma'aruf Mohammed Adamu <63424383+maarufadam@users.noreply.github.com>
2022-02-11 17:02:43 +0100

    template

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'user_id',
+    ];
+
+    protected $cast = [
+        'user_id' => 'integer',
+    ];
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Project1</title>
+
+    <!-- Bootstrap 4 -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
+
+</head>
+
+<body class="bg-light text-dark mt-2">
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="#">Home</a>
+        <button type="button" class="btn mr-1">Posts</button>
+        <button type="button" class="btn mr-auto">Dashboard</button>
+
+        <li class="nav-item">
+            <a class="nav-link text-light" href="#">Maaruadam</a>
+        </li>
+        <button type="button" class="btn mr-1">Login</button>
+        <button type="button" class="btn">Register</button>
+    </nav>
+
+
+    <!-- Font Awesome -->
+    <script src="{{ asset('js/all.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <!-- JQuery -->
+    <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+    <!-- JQuery Validation -->
+    <script src="{{ asset('js/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('js/additional-methods.min.js') }}"></script>
+</body>
+
+</html>

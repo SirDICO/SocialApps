84145bf4c208ed4773a9b6ab14f26038e9c399ef
Ma'aruf Mohammed Adamu <63424383+maarufadam@users.noreply.github.com>
2022-02-11 17:08:44 +0100

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Project1</title>
+    <title>Laravel Class</title>
 
     <!-- Bootstrap 4 -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
